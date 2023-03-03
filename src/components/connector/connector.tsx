7bd046be8d0c521f96e3c6bda59b3a4fae87b049
Abbas Yassini <yassini.abbas.dev@gmail.com>
2023-03-03 19:58:13 +0330

    added svg components as memo for preventing unnecessary renders

--- src/components/connector/connector.tsx
+++ src/components/connector/connector.tsx
@@ -1,15 +1,24 @@
+import { memo } from 'react';
 import { ConnectorProps } from './connector.interface';
 
-export const Connector = ({ sourcePoint, targetPoint }: ConnectorProps) => {
-  return (
-    <line
-      x1={sourcePoint.x}
-      y1={sourcePoint.y}
-      x2={targetPoint.x}
-      y2={targetPoint.y}
-      stroke-width='1'
-      strokeDasharray='10 5'
-      stroke='grey'
-    />
-  );
-};
+export const Connector = memo(
+  ({ sourcePoint, targetPoint }: ConnectorProps) => {
+    console.log('XX@XX', sourcePoint, targetPoint);
+    return (
+      <line
+        x1={sourcePoint.x}
+        y1={sourcePoint.y}
+        x2={targetPoint.x}
+        y2={targetPoint.y}
+        stroke-width='1'
+        strokeDasharray='10 5'
+        stroke='grey'
+      />
+    );
+  },
+  (prev, next) => {
+    return !(
+      prev.sourcePoint.x !== next.sourcePoint.x || prev.sourcePoint.y !== next.sourcePoint.y
+    );
+  }
+);
--- src/components/node/node.tsx
+++ src/components/node/node.tsx
@@ -1,31 +1,42 @@
+import { memo } from 'react';
 import { NodeProps } from './node.interface';
 import { useNode } from './use-node';
 
-export const Node = ({ label, position, size }: NodeProps) => {
-  const { truncateLabel } = useNode(size);
-  const truncatedLabel = truncateLabel(label);
-  const ratio = truncatedLabel === label ? 2 : 3;
+export const Node = memo(
+  ({ label, position, size }: NodeProps) => {
+    const { truncateLabel } = useNode(size);
+    const truncatedLabel = truncateLabel(label);
+    const ratio = truncatedLabel === label ? 2 : 3;
 
-  return (
-    <>
-      <circle
-        cx={position.x}
-        cy={position.y}
-        r={size - 2}
-        fill='grey'
-        stroke='yellow'
-        strokeWidth='2px'
-      />
+    return (
+      <>
+        <circle
+          cx={position.x}
+          cy={position.y}
+          r={size - 2}
+          fill='grey'
+          stroke='yellow'
+          strokeWidth='2px'
+        />
 
-      <text
-        x={`${position.x - (truncatedLabel.length * 7.8) / ratio}`}
-        y={position.y + 3}
-        fill='black'
-        fontSize='14px'
-      >
-        <title>{label}</title>
-        {truncatedLabel}
-      </text>
-    </>
-  );
-};
+        <text
+          x={`${position.x - (truncatedLabel.length * 7.8) / ratio}`}
+          y={position.y + 3}
+          fill='black'
+          fontSize='14px'
+        >
+          <title>{label}</title>
+          {truncatedLabel}
+        </text>
+      </>
+    );
+  },
+  (prev, next) => {
+    return !(
+      prev.position.x !== next.position.x ||
+      prev.position.y !== next.position.y ||
+      prev.label !== next.label ||
+      prev.size !== next.size
+    );
+  }
+);
